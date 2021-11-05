ef7475512b6fd5790e9082f5a9dfe3639afcd7ba
pierfrancescoscar <scarinci401@gmail.com>
2021-11-05 14:41:41 +0100

    add array with 5 objects for our cards

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
 // - Creiamo il nostro array di 5 oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine, numero di likes.
+
+// 1. Creiamo il nostro array di 5 oggetti
+
+const socialPost = [
+
+    {
+        authorName: 'Pierfrancesco Scarinci',
+        profilePic: 'https://picsum.photos/seed/picsum/200/300',
+        date: '2 Gennaio 1995',
+        postTxt: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+        postPic: 'https://picsum.photos/seed/picsum/200/300',
+        likesNum: 95,
+
+    },
+
+    {
+        authorName: 'Pietro Scaringi',
+        profilePic: 'https://picsum.photos/seed/picsum/200/300',
+        date: '2 Gennaio 1995',
+        postTxt: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+        postPic: 'https://picsum.photos/seed/picsum/200/300',
+        likesNum: 105,
+
+    },
+
+    {
+        authorName: 'Pietrofranco Scarci',
+        profilePic: 'https://picsum.photos/seed/picsum/200/300',
+        date: '2 Gennaio 1995',
+        postTxt: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+        postPic: 'https://picsum.photos/seed/picsum/200/300',
+        likesNum: 100,
+
+    },
+
+    {
+        authorName: 'Piero Scarnici',
+        profilePic: 'https://picsum.photos/seed/picsum/200/300',
+        date: '2 Gennaio 1995',
+        postTxt: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+        postPic: 'https://picsum.photos/seed/picsum/200/300',
+        likesNum: 100,
+
+    },
+
+    {
+        authorName: 'Pietro Scarnigi',
+        profilePic: 'https://picsum.photos/seed/picsum/200/300',
+        date: '2 Gennaio 1995',
+        postTxt: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+        postPic: 'https://picsum.photos/seed/picsum/200/300',
+        likesNum: 50,
+
+    },
+    
+    ];
+
+    console.log(socialPost);
+
+    // 2. Stampare le card 
